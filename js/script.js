b318d8ee9b140173c3c02d9b0b30d9e21a0b5d4f
minexa-srl <erisesucre@gmail.com>
2026-05-05 20:18:31 -0400

    UND-01: Fix logo

--- js/script.js
+++ js/script.js
@@ -8,13 +8,17 @@
 ----------------------- */
 var navBoton = document.getElementById('navBoton');
 var navMenu = document.getElementById('navMenu');
+var enlace1 = document.getElementById('enlace1');
+var enlace2 = document.getElementById('enlace2');
+var enlace3 = document.getElementById('enlace3');
+
 
 /* -----------------------
       FUNCIONES
 ----------------------- */
 
 function mostrarOcultarNav() {
-  navMenu.classList.toggle('ocultarNavMenu');
+      navMenu.classList.toggle('ocultarNavMenu');
 }
 
 /* -----------------------
@@ -23,3 +27,6 @@ function mostrarOcultarNav() {
 
 
 navBoton.addEventListener('click', mostrarOcultarNav);
+enlace1.addEventListener('click', mostrarOcultarNav);
+enlace2.addEventListener('click', mostrarOcultarNav);
+enlace3.addEventListener('click', mostrarOcultarNav);
